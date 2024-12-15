651e6652c7629f5ef601db3675f18cf9c919eca0
YashShrivastava14 <150028448+YashShrivastava14@users.noreply.github.com>
2024-12-15 20:52:24 +0000

    learned variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const rollNum = 204
+let email = "yshri14@gmail.com"
+var pass = 12345
+address = "govindpuri"
+
+// rollNum = 34            // (Assignment to constant) is not accepted
+// let email = "hahaha"        // 'let' can not be declared again , only reassigned
+var pass = 1444             // 'var' can be declared again as well as reassigned
+address = "thatipur"
+
+/*
+"Var is not used anymore."
+*/
+
+console.table([rollNum,email,pass, address])
