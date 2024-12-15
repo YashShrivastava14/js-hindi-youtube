b36798567df09c0f3d4b18bb7df3b5ba160e9db1
YashShrivastava14 <150028448+YashShrivastava14@users.noreply.github.com>
2024-12-15 21:49:25 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,8 @@ const rollNum = 204
 let email = "yshri14@gmail.com"
 var pass = 12345
 address = "govindpuri"
+let  city ;
+
 
 // rollNum = 34            // (Assignment to constant) is not accepted
 // let email = "hahaha"        // 'let' can not be declared again , only reassigned
@@ -9,7 +11,7 @@ var pass = 1444             // 'var' can be declared again as well as reassigned
 address = "thatipur"
 
 /*
-"Var is not used anymore."
+"Var is not used anymore, because of issue in block scope and fnctnal scope"
 */
 
-console.table([rollNum,email,pass, address])
+console.table([rollNum,email,pass, address, city])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";   // by this all the code is treated as per the new version of JS
+
+// alert("yash");      // we are using Node js not Browser
+
+//DATATYPES :-
+let number = 5;             //number
+// bigInt    ---   for very large number
+let fullname = "yashshrivastava"  //string
+let isLoggedIn = true           //boolean
+let data = null;        // standalone value  -- object datatype
+let state ;             //undefined
+//  symbol => unique
+
+
+console.table(typeof undefined );       //undefined
+console.table(typeof null );            //object
+
