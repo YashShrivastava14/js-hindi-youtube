d4ce798d4fb4c41dfc9c24c5b55813101c7a9469
YashShrivastava14 <150028448+YashShrivastava14@users.noreply.github.com>
2024-12-16 19:40:06 +0000

    conversion_of_datatypes

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,42 @@
+let score = "yash12";   //string
+// let newScore = Number(score)
+  newScore = Boolean(score)
+  // newScore = NaN
+  // newScore = undefined
+
+// console.log(newScore);
+
+let age = 10
+let newAge = String(age)
+    newAge = Boolean(age)
+    newAge = NaN
+
+
+    let some = NaN
+    newsome = String(some)
+    // console.log(newsome);
+// console.log(typeof newsome);
+
+
+let bullu = true
+let newbullu = Number(bullu)
+// console.log(newbullu);
+
+
+console.log( newAge)
+
+console.log(typeof age + " --> " + typeof newAge ); 
+
+/*
+
+"yash123" ---number---> NaN
+undefined ---number---> NaN
+null ---number---> 0
+NaN ---number--->  NaN
+string ---boolean--->  true
+empty string "" ---boolean--->  false
+0 ---boolean--->  False
+25 ---boolean--->  true
+
+
+*/
