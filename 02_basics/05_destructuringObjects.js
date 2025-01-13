133991c0d9bbdc3346736a33c77b6e113bafbb1a
YashShrivastava14 <150028448+YashShrivastava14@users.noreply.github.com>
2025-01-13 00:11:58 +0000

    Destructuring and JSON

---
+++ 02_basics/05_destructuringObjects.js
@@ -0,0 +1,20 @@
+const userObject = {
+    username : "yash",
+    id : 204,
+    course : "BTech"
+}
+
+// console.log(userObject);
+
+const {username,id,course} = userObject     //Destructuring
+
+console.log(`name is ${username} id is ${id} course is ${course}   `);
+
+
+// ------ JSON ------------
+
+// {
+//     "name" : "yash",
+//     "rollNumber" : 204,
+//     "city" : "Orai"
+// }
