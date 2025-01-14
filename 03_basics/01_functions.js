f57df91edede3c716e777b8f6bd96399d2fdd91e
YashShrivastava14 <150028448+YashShrivastava14@users.noreply.github.com>
2025-01-14 22:55:50 +0000

    more on functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -13,3 +13,59 @@ const addingTwoNumbers = function (num1, num2) {
 };
 
 console.log(addingTwoNumbers(2, "5")); // calling has arguments
+
+
+// DEFAULT ARGUMENT
+const defaultArgu = function (num = 1){
+    return `the default value in the argument is ${num}`;
+}
+
+console.log(defaultArgu());
+
+
+// REST AND SPREAD OPERATORS
+// both are same(...var) , depends upon the use case when it'd be called spread and when rest
+
+// const myfunc = function(...num){        //rest operator
+//      console.log(num);     
+// }
+const myfunc = function(var1,var2,...num){        //rest operator
+    console.log(num);     
+}
+
+myfunc(1444,234,455,67)
+
+
+const myObject = {
+    name : "yash",
+    id : 204
+}
+
+
+// PASSING OBJECT AS ARGUMENT
+
+// const objTakingFunction = function(obj){       
+//     console.log(`obj.name is ${obj.name} and obj.id is ${obj.id}`);     
+// }
+
+// objTakingFunction(myObject)
+
+const objTakingFunction = function(obj){       
+    console.log(`obj.name is ${obj.name} and obj.id is ${obj.id}`);     
+}
+objTakingFunction({
+    name : "sam",
+    id : 123
+})
+
+
+
+// PASSING ARRAY AS ARGUMENT
+const myArray =[12,24,588,90]
+
+const returnSecondElement = function (arguarray){
+    console.log(arguarray[1]);   
+}
+
+returnSecondElement(myArray)
+returnSecondElement([10,20,30,40])
